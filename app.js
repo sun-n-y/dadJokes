4d5d26dae72ebbd998cf897887d923ba2000071e
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-07 15:31:50 -0400

    add dad jokes js complete

---
+++ app.js
@@ -0,0 +1,29 @@
+//
+const url = 'https://icanhazdadjoke.com';
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+btn.addEventListener('click', () => {
+  displayJoke();
+});
+
+const displayJoke = async () => {
+  result.textContent = 'loading...';
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'User-Agent': 'learning app',
+      },
+    });
+    // if (!response.ok) {
+    //   throw new Error('error in url');
+    // }
+    const data = await response.json();
+    result.textContent = data.joke;
+  } catch (error) {
+    result.textContent = 'Error...';
+  }
+};
+
+displayJoke();
